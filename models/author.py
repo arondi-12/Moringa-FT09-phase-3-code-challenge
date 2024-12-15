85129677eb4ddb4e814b2d2a215be655650f8e03
arondi-12 <davieariwi@gmail.com>
2024-12-15 23:51:16 +0300

    Initial commit

--- models/author.py
+++ models/author.py
@@ -1,7 +1,35 @@
+from database.connection import get_db_connection
+
 class Author:
     def __init__(self, id, name):
         self.id = id
         self.name = name
+        
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO authors (name) VALUES (?)",
+            (name,)
+        )
+        conn.commit()
+        self.id = cursor.lastrowid
+        conn.close()
+
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def name(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT name FROM authors WHERE id = ?",
+            (self._id,)
+        )
+        self._name = cursor.fetchone()[0]
+        conn.close()
+        return self._name
 
     def __repr__(self):
         return f'<Author {self.name}>'
